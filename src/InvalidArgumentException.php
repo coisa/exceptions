c25b8823576a7dc40aa644b56b3d40c93c80174e
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-09-25 13:33:52 -0300

    CS-Fix

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -18,7 +18,9 @@ namespace CoiSA\Exception;
  *
  * @package CoiSA\Exception
  */
-class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface, ExceptionFactoryInterface
+class InvalidArgumentException extends \InvalidArgumentException implements
+    ExceptionInterface,
+    ExceptionFactoryInterface
 {
     /** @const string */
     const TEMPLATE_EXPECTED_CALLABLE_ARGUMENT = 'Given argument "%s" are not a valid callable function.';
